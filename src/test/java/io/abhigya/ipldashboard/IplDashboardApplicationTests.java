6e3383120fb7b141f982f5ff0c53df5fc465d05e
abhigya.sa <abhigya.sa@gmail.com>
2021-11-24 17:00:39 -0500

    Added Team API endpoint

---
+++ src/test/java/io/abhigya/ipldashboard/IplDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package io.abhigya.ipldashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IplDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
