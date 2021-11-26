e455e2eba5a67c65ae67b6a2ea3c74668ae71df3
abhigya.sa <abhigya.sa@gmail.com>
2021-11-25 21:18:21 -0500

    Add backend Match API

--- src/main/java/io/abhigya/ipldashboard/controller/TeamController.java
+++ src/main/java/io/abhigya/ipldashboard/controller/TeamController.java
@@ -1,10 +1,15 @@
 package io.abhigya.ipldashboard.controller;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.abhigya.ipldashboard.model.Match;
 import io.abhigya.ipldashboard.model.Team;
 import io.abhigya.ipldashboard.repository.MatchRepository;
 import io.abhigya.ipldashboard.repository.TeamRepository;
@@ -26,6 +31,19 @@ public class TeamController {
         Team team = this.teamRepository.findByTeamName(teamName);
         team.setMatches(matchRepository.findLatestMatchesByTeam(teamName, 4));
         return team;
+    }
+
+    @GetMapping("/team/{teamName}/matches")
+    public List<Match> getMatchesForTeam(@PathVariable String teamName, @RequestParam int year) {
+        LocalDate startDate = LocalDate.of(year, 1, 1);
+        LocalDate endDate = LocalDate.of(year + 1, 1, 1);
+        return this.matchRepository.getByTeam1AndDateBetweenOrTeam2AndDateBetweenOrderByDateDesc(
+            teamName,
+            startDate,
+            endDate,
+            teamName,
+            startDate,
+            endDate);
     }
 
 
--- src/main/java/io/abhigya/ipldashboard/repository/MatchRepository.java
+++ src/main/java/io/abhigya/ipldashboard/repository/MatchRepository.java
@@ -1,5 +1,6 @@
 package io.abhigya.ipldashboard.repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.domain.PageRequest;
@@ -12,6 +13,7 @@ public interface MatchRepository extends CrudRepository<Match, Long> {
     
     List<Match> getByTeam1OrTeam2OrderByDateDesc(String teamName1, String teamName2, Pageable pageable);
     
+    List<Match> getByTeam1AndDateBetweenOrTeam2AndDateBetweenOrderByDateDesc(String teamName1,  LocalDate date1, LocalDate date2, String teamName2, LocalDate date3, LocalDate date4);
     default List<Match> findLatestMatchesByTeam(String teamName, int count) {
         return getByTeam1OrTeam2OrderByDateDesc(teamName, teamName, PageRequest.of(0, count));
     } 
