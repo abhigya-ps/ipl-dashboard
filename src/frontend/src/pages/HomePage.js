0cf30cfcd5ac1551ae5e0eb8f56a222dbbdfb22b
abhigya.sa <abhigya.sa@gmail.com>
2021-11-27 00:23:09 -0500

    Match Page year selector and Home Page added

---
+++ src/frontend/src/pages/HomePage.js
@@ -0,0 +1,34 @@
+import { React, useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { Link } from "react-router-dom";
+import { MatchDetailCard } from "../components/MatchDetailCard";
+import { MatchSmallCard } from "../components/MatchSmallCard";
+import { PieChart } from 'react-minimal-pie-chart';
+
+import "./HomePage.scss";
+import { TeamTile } from "../components/TeamTile";
+
+export const HomePage = () => {
+  const [teams, setTeams] = useState([]);
+
+  useEffect(() => {
+    const fetchAllTeams = async () => {
+      const response = await fetch(`http://localhost:8090/team`);
+      const data = await response.json();
+      console.log(data);
+      setTeams(data);
+    };
+    fetchAllTeams();
+  }, []);
+
+  return (
+    <div className="HomePage">
+      <div className="header-section">
+        <h1 className="app-name">IPL Cricket Dashboard</h1>
+      </div>
+      <div className="team-grid">
+          { teams.map(team => <TeamTile teamName={team.teamName} />)}
+      </div>
+    </div>
+  );
+};
--- src/frontend/src/pages/TeamPage.js
+++ src/frontend/src/pages/TeamPage.js
@@ -1,5 +1,6 @@
 import { React, useEffect, useState } from "react";
 import { useParams } from "react-router";
+import { Link } from "react-router-dom";
 import { MatchDetailCard } from "../components/MatchDetailCard";
 import { MatchSmallCard } from "../components/MatchSmallCard";
 import { PieChart } from 'react-minimal-pie-chart';
@@ -11,13 +12,13 @@ export const TeamPage = () => {
   const { teamName } = useParams();
 
   useEffect(() => {
-    const fetchMatches = async () => {
+    const fetchTeam = async () => {
       const response = await fetch(`http://localhost:8090/team/${teamName}`);
       const data = await response.json();
       console.log(data);
       setTeam(data);
     };
-    fetchMatches();
+    fetchTeam();
   }, [teamName]);
 
   if (!team || !team.teamName) {
@@ -46,7 +47,7 @@ export const TeamPage = () => {
         <MatchSmallCard teamName={team.teamName} match={match} />
       ))}
       <div className="more-link">
-        <a href="#">More ></a>
+      <Link to={`/teams/${teamName}/matches/${process.env.REACT_APP_DATA_END_YEAR}`}>More ></Link>
       </div>
     </div>
   );
